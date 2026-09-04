b46817592d36480af5640b441d29573c552ba0ed
p3rtub4d00 <containerbeerpvh@gmail.com>
2026-09-04 16:30:44 -0400

    fix: import React namespace to resolve production JSX runtime error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import { Users, Package, ShoppingCart, FileSignature, Wallet, MessageCircle, CheckCircle2 } from 'lucide-react';
 
